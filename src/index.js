2c3d119bc7a23f44d4fce6e0bd1939369901ba27
weiminfu <weiminfu@126.com>
2019-06-01 18:52:43 +0800

    React脚手架

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import qs from 'qs';
+import './static/less/index.less';
+
+console.log(qs.parse('name=wei&age=12')); //  {name: "wei", age: "12"}
+
+
+
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
